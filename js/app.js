8b13ef27553209245ce0383312d41e9619c99073
Ian Gilmore <ian.gilmore@me.com>
2024-03-19 09:46:08 -0400

    add playTimeDisplay variable update logic to updateTimer function, improve temporary testing function logic

--- js/app.js
+++ js/app.js
@@ -19,7 +19,8 @@ const levels = {
 let today = new Date().toLocaleDateString()
 let chosenLevel
 let gameLength = 25
-let playTime
+let playTimeSeconds
+let playTimeDisplay
 let results = []
 let answeredCount
 let progress
@@ -27,11 +28,17 @@ let score
 let accuracy
 
 /*------------------------ Cached Element References ------------------------*/
+// Landing
+const landingEl = document.querySelector('.landing')
 const dateEl = document.querySelector('.date')
 const easyBtn = document.querySelector('.easy-btn')
 const medBtn = document.querySelector('.med-btn')
 const hardBtn = document.querySelector('.hard-btn')
 
+// Game
+const timerEL = document.querySelector('.timer')
+
+// Results
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -43,7 +50,8 @@ function init() {
   easyBtn.textContent = levels.easy.name
   medBtn.textContent = levels.medium.name
   hardBtn.textContent = levels.hard.name
-  playTime = 0
+  playTimeSeconds = 0
+  // timerEL.textContent = playTimeDisplay
   results = []
   score = 0
   render()
@@ -56,15 +64,19 @@ function render() {
 }
 
 function startTimer(params) {
-  counter = setInterval(updateTimer, 1000);
+  counter = setInterval(updateTimer, 1000)
 }
 
 function updateTimer() {
-  playTime += 1
+  playTimeSeconds += 1
+  const minutes = Math.floor(playTimeSeconds / 60)
+  const seconds = playTimeSeconds % 60
+  playTimeDisplay = minutes.toString().padStart(1, '0') + ':' + seconds.toString().padStart(2, '0')
+  console.log(`playTimeDisplay: ${playTimeDisplay}`)
 }
 
 function stopTimer() {
-  clearInterval(counter);
+  clearInterval(counter)
 }
 
 function updateStats() {
@@ -80,13 +92,18 @@ function updateStats() {
 
 /*-------------------------------- Temp functions for testing --------------------------------*/
 
+function demoInConsole() {
+  seedProgress()
+  startTimer()
+  logVariables()
+}
+
 function seedProgress(params) {
   results.push(1,0,0,1,1,1,0,1,0,1)
 }
 
 function logVariables() {
-  seedProgress()
-  updateScore()
+  updateStats()
   console.log(`results: ${results}`);
   console.log(`answeredCount: ${answeredCount}`);
   console.log(`progress: ${progress}`);
