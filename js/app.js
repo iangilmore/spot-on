5ce180aefd9bc68620c449e914c08ed546ed5ab8
Ian Gilmore <ian.gilmore@me.com>
2024-03-20 08:14:14 -0400

    add preRender, finalRender, hideLanding, showGame, and delay functions, also add a few new variables

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
 const levels = {
-  "easy": {
+  "0": {
     "value": 0,
+    "difficulty": "easy",
     "name": "Newb"
   },
-  "medium": {
+  "1": {
     "value": 1,
+    "difficulty": "medium",
     "name": "Seasoned"
   },
-  "hard": {
+  "2": {
     "value": 2,
+    "difficulty": "hard",
     "name": "Savant"
   }
 }
@@ -18,52 +21,83 @@ const levels = {
 /*---------------------------- Variables (state) ----------------------------*/
 let today = new Date().toLocaleDateString()
 let chosenLevel
-let gameLength = 25
+let gameLength = 10
 let playTimeSeconds
 let playTimeDisplay
 let results = []
 let answeredCount
+let remainingCount
 let progress
 let score
 let accuracy
 
 /*------------------------ Cached Element References ------------------------*/
-// Landing
+/* Landing */
 const landingEl = document.querySelector('.landing')
 const dateEl = document.querySelector('.date')
 const easyBtn = document.querySelector('.easy-btn')
 const medBtn = document.querySelector('.med-btn')
 const hardBtn = document.querySelector('.hard-btn')
 
-// Game
+/* Game */
+const gameEl = document.querySelector('.game')
+const levelEl = document.querySelector('.level')
 const timerEL = document.querySelector('.timer')
+const statsEl = document.querySelector('.stats')
+const completeEl = document.querySelector('.complete')
+const incompleteEl = document.querySelector('.incomplete')
+const answerAEl = document.querySelector('#answer-a button')
+const answerBEl = document.querySelector('#answer-b button')
+const imageEl = document.querySelector('.image img')
 
-// Results
+/* Results */
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+easyBtn.addEventListener('click',finalRender)
+medBtn.addEventListener('click',finalRender)
+hardBtn.addEventListener('click',finalRender)
 
 
 /*-------------------------------- Functions --------------------------------*/
 function init() {
   dateEl.textContent = today
-  easyBtn.textContent = levels.easy.name
-  medBtn.textContent = levels.medium.name
-  hardBtn.textContent = levels.hard.name
+  easyBtn.textContent = levels[0].name
+  medBtn.textContent = levels[1].name
+  hardBtn.textContent = levels[2].name
   playTimeSeconds = 0
-  // timerEL.textContent = playTimeDisplay
+  playTimeDisplay = "0:00"
   results = []
   score = 0
-  render()
+  preRender()
 }
 
 init()
 
-function render() {
-
+function preRender() {
+  timerEL.textContent = playTimeDisplay
+  completeEl.flexGrow = 0
+  incompleteEl.flexGrow = gameLength
+  answerAEl.textContent = "Pear"
+  answerBEl.textContent = "Apple"
+  imageEl.src = "/img/apple.jpeg"
 }
 
-function startTimer(params) {
+async function finalRender(click) {
+  chosenLevel = click.target.id
+  console.log(click.target.id);
+  levelEl.textContent = levels[chosenLevel].name
+  hideLanding()
+  await delay(1000)
+  showGame()
+  await delay(1000)
+  startGame()
+}
+
+function startGame() {
+  startTimer()
+}
+
+function startTimer() {
   counter = setInterval(updateTimer, 1000)
 }
 
@@ -72,7 +106,7 @@ function updateTimer() {
   const minutes = Math.floor(playTimeSeconds / 60)
   const seconds = playTimeSeconds % 60
   playTimeDisplay = minutes.toString().padStart(1, '0') + ':' + seconds.toString().padStart(2, '0')
-  console.log(`playTimeDisplay: ${playTimeDisplay}`)
+  timerEL.textContent = playTimeDisplay
 }
 
 function stopTimer() {
@@ -81,6 +115,10 @@ function stopTimer() {
 
 function updateStats() {
   answeredCount = results.length
+  // completeEl.
+  completeEl.textContent = answeredCount
+  remainingCount = gameLength - answeredCount
+  incompleteEl.textContent = remainingCount
   progress = answeredCount / gameLength
   score = results.reduce((prev,curr) => {
     return prev + curr
@@ -88,7 +126,21 @@ function updateStats() {
   accuracy = score / answeredCount
 }
 
+function hideLanding() {
+  landingEl.classList.add('fade-out-1s')
+}
 
+function showGame() {
+  gameEl.classList.add('fade-in-1s')  
+}
+
+/*-------------------------------- Helper functions  --------------------------------*/
+
+function delay(milliseconds){
+  return new Promise(resolve => {
+      setTimeout(resolve, milliseconds);
+  });
+}
 
 /*-------------------------------- Temp functions for testing --------------------------------*/
 
